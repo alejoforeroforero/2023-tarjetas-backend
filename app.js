736ac50be31cb3900cfb1c1918c71d860f33ba5e
alejoforeroforero <alejoforeroforero@gmail.com>
2023-06-12 08:17:46 -0500

    sin mongoose

--- app.js
+++ app.js
@@ -8,18 +8,16 @@ app.get("/", (req, res, next) => {
   res.send("Hola mis mejores amigos");
 });
 
-mongoose.connect(baseDeDatos, {
-  useNewUrlParser: true,
-});
+app.listen(process.env.PORT || 3400);
 
-const db = mongoose.connection;
-db.on("error", (error) => {
-  console.error(error);
-});
-db.once("open", () => {
-  console.log("conectado a Mongoose");
+// const db = mongoose.connection;
+// db.on("error", (error) => {
+//   console.error(error);
+// });
+// db.once("open", () => {
+//   console.log("conectado a Mongoose");
 
-  app.listen(process.env.PORT || 3400);
-});
+//   app.listen(process.env.PORT || 3400);
+// });
 
 //
