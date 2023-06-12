df7d9b8e007e20eb395a55db3aa2228301026d5d
alejoforeroforero <alejoforeroforero@gmail.com>
2023-06-12 08:19:34 -0500

    sin env.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { baseDeDatos } = require("./env.js");
+//const { baseDeDatos } = require("./env.js");
 
 const app = express();
 
