4c91179ae36d48545df6c7ac182ba1de2b8f770f
alejoforeroforero <alejoforeroforero@gmail.com>
2023-06-12 08:13:46 -0500

    Con Env

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const { baseDeDatos } = require("./env.js");
 
 const app = express();
 
-app.get('/', (req, res, next)=>{
+app.get("/", (req, res, next) => {
+  res.send("Hola mis mejores amigos");
+});
 
-    res.send('Hola mis mejores amigazos');
-})
+mongoose.connect(baseDeDatos, {
+  useNewUrlParser: true,
+});
 
-app.listen(process.env.PORT || 3400);
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.error(error);
+});
+db.once("open", () => {
+  console.log("conectado a Mongoose");
+
+  app.listen(process.env.PORT || 3400);
+});
+
+//
