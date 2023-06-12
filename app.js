be95a0e796568a965e9ec5e796753f6f1b833639
alejoforeroforero <alejoforeroforero@gmail.com>
2023-06-12 12:33:45 -0500

    con .env

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 const app = express();
 
 app.get("/", (req, res, next) => {
-  
+  res.send(process.env.BASE);
 });
 
 
