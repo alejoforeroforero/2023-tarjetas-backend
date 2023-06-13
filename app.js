cde5d6c9192255ec9c1f1432eb28acb38fe6777e
alejoforeroforero <alejoforeroforero@gmail.com>
2023-06-12 19:25:23 -0500

    prueba

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const n = 2;
+
 const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next) => {
