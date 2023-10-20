542e5b6f96fd7f65142125dd7f09128c2be8eccc
alejoforeroforero <alejoforeroforero@gmail.com>
2023-10-19 21:55:03 -0500

    Cambio total

--- app.js
+++ app.js
@@ -1,32 +1,66 @@
-//require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const usuarioRoutes = require("./routes/usuario-routes");
-const tarjetasRoutes = require("./routes/tarjeta-routes");
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
+
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
   next();
 });
 
-app.use("/api/usuarios", usuarioRoutes);
-app.use("/api/tarjetas", tarjetasRoutes);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
 
 mongoose
-  .connect(process.env.BASE)
+  .connect(
+    //`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.ehnrfax.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    //`mongodb+srv://alejoforeroforero:5LHGjoHldObc8GjB@cluster1.ehnrfax.mongodb.net/mern?retryWrites=true&w=majority`
+    process.env.DATABASE_URL
+  )
   .then(() => {
-    app.listen(process.env.PORT || 3400);
-    console.log("conecto");
+    app.listen(5000);
+    console.log("entra");
   })
-  .catch((error) => {
-    console.log("Algo paso");
+  .catch(err => {
+    console.log(err);
   });
